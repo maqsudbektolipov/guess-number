7568823daf32a3161327a1fbaaa80812b334d8d2
MAQSUDBEK <72947085+maqsudbektolipov@users.noreply.github.com>
2020-12-29 23:36:50 +0500

    1

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess !== secretNumber) {
     if (score > 1) {
       displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
       document.querySelector('.score').textContent = score;
     } else {
       displayMessage('💥 You lost the game!');
